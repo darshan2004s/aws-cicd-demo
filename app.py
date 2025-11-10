e6c632f79ecbb311e959f22cca03e50f24ff6297
DARSHAN S <161106946+darshan2004s@users.noreply.github.com>
2025-11-10 11:40:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello from Jenkins + Docker Pipeline!"
+def home():
+    return "Hello from Jenkins CI/CD Pipeline on AWS!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
